2bd320b10cbec31d6cc0ffa4969c11501fdb40e9
aohiro96 <51246825+aohiro96@users.noreply.github.com>
2019-09-12 23:22:50 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+$(function(){
+
+
+
+  // ヘッダー内の<a>タグをクリックしたときのclickイベントを作成してください。
+  $('.list h3').click(function(){
+    var id = $(this).attr("href")
+    var position = $(id).offset().top;
+    $('html,body').animate({
+      'scrollTop': position
+    }, 500);
+  }
+    );
+    $('.change-modal h4').click(function(){
+      var id = $(this).attr("href")
+      var position = $(id).offset().top;
+      $('html,body').animate({
+        'scrollTop': position
+      }, 500);
+      $('.change-modal-wrapper').fadeOut();
+    }
+      );
+
+
+    $('.ver2').click(function() {
+      $('.change-modal-wrapper').fadeIn();
+    });
+
+    $('.close-modal').click(function() {
+      $('.change-modal-wrapper').fadeOut();
+    });
+
+});
