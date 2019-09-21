599c0e8416c40554e63661e92c1efa1f62953ba3
aohiro96 <51246825+aohiro96@users.noreply.github.com>
2019-09-22 01:19:57 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ $(function(){
 
 
 
-
+//スクロール系
   $('.list h3').click(function(){
     var id = $(this).attr("href")
     var position = $(id).offset().top;
-    
+
     $('html,body').animate({
       'scrollTop': position
     }, 500);
@@ -22,8 +22,16 @@ $(function(){
     }
       );
 
-
+//modal系
     $('.ver2').click(function() {
+      $('.change-modal-wrapper').fadeIn();
+    });
+
+    $('.close-modal').click(function() {
+      $('.change-modal-wrapper').fadeOut();
+    });
+    //スマホ用
+    $('.menu-icon').click(function() {
       $('.change-modal-wrapper').fadeIn();
     });
 
